1198a3747f8fb38b899c5dce6e6063020c253a34
CoolhanD <abrawa@gmail.com>
2022-04-27 23:44:18 +0300

    nav scritp and burger

---
+++ scripts/nav_script.js
@@ -0,0 +1,16 @@
+const menuBtn = document.querySelector(".nav__burger");
+const navigation = document.querySelector(".nav");
+const navMenu = document.querySelector(".nav__menu");
+const navLinks = document.querySelector(".nav__links");
+const navBtnBts = document.querySelector(".nav_button_btc");
+const navBtnDollar = document.querySelector(".nav_button_dollar");
+
+menuBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  menuBtn.classList.toggle("is-active");
+  navigation.classList.toggle("is-active");
+  navMenu.classList.toggle("is-active");
+  navLinks.classList.toggle("is-active");
+  navBtnBts.classList.toggle("is-active");
+  navBtnDollar.classList.toggle("is-active");
+});
